c0c40b949364093973c18ff447a6d0298d101a98
Denis Sheremet <teenmind1408@gmail.com>
2017-10-26 16:58:19 +0300

    Initial commit

---
+++ UnitTestCalc/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestCalc")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestCalc")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0785e859-3bb3-4cf2-a783-639755e63b6b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestCalc/UnitTestRealJob.cs
@@ -0,0 +1,74 @@
+﻿using TestStack.White;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.Factory;
+using TestStack.White.UIItemEvents;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestFieldsCalc
+{
+    [TestClass]
+    public class UnitTestFields
+    {
+        [TestMethod]
+        public void TestMethodPlus()
+        {
+            Application application = Application.Launch("C:\\my stuff\\ORT courses\\Homeworks\\WpfApplicationCalc\\WpfApplicationCalc\\obj\\Debug\\WpfApplicationCalc.exe");
+            Window window = application.GetWindow("MainWindow", InitializeOption.NoCache);
+
+            window.Get<TextBox>("aTxt").Text = "5";
+            window.Get<TextBox>("bTxt").Text = "5";
+            window.Get<TextBox>("oPer").Text = "+";
+            window.Get<Button>("res").Click();
+            string res = window.Get<TextBox>("resTxt").Text;
+            application.Kill();
+            Assert.AreEqual("10", res);
+        }
+
+        [TestMethod]
+        public void TestMethodMinus()
+        {
+            Application application = Application.Launch("C:\\my stuff\\ORT courses\\Homeworks\\WpfApplicationCalc\\WpfApplicationCalc\\obj\\Debug\\WpfApplicationCalc.exe");
+            Window window = application.GetWindow("MainWindow", InitializeOption.NoCache);
+
+            window.Get<TextBox>("aTxt").Text = "5";
+            window.Get<TextBox>("bTxt").Text = "5";
+            window.Get<TextBox>("oPer").Text = "-";
+            window.Get<Button>("res").Click();
+            string res = window.Get<TextBox>("resTxt").Text;
+            application.Kill();
+            Assert.AreEqual("0", res);
+        }
+
+        [TestMethod]
+        public void TestMethodMult()
+        {
+            Application application = Application.Launch("C:\\my stuff\\ORT courses\\Homeworks\\WpfApplicationCalc\\WpfApplicationCalc\\obj\\Debug\\WpfApplicationCalc.exe");
+            Window window = application.GetWindow("MainWindow", InitializeOption.NoCache);
+
+            window.Get<TextBox>("aTxt").Text = "5";
+            window.Get<TextBox>("bTxt").Text = "5";
+            window.Get<TextBox>("oPer").Text = "*";
+            window.Get<Button>("res").Click();
+            string res = window.Get<TextBox>("resTxt").Text;
+            application.Kill();
+            Assert.AreEqual("25", res);
+        }
+
+        [TestMethod]
+        public void TestMethodDiv()
+        {
+            Application application = Application.Launch("C:\\my stuff\\ORT courses\\Homeworks\\WpfApplicationCalc\\WpfApplicationCalc\\obj\\Debug\\WpfApplicationCalc.exe");
+            Window window = application.GetWindow("MainWindow", InitializeOption.NoCache);
+
+            window.Get<TextBox>("aTxt").Text = "10";
+            window.Get<TextBox>("bTxt").Text = "5";
+            window.Get<TextBox>("oPer").Text = "/";
+            window.Get<Button>("res").Click();
+            string res = window.Get<TextBox>("resTxt").Text;
+            application.Kill();
+            Assert.AreEqual("2", res);
+        }
+    }
+}
